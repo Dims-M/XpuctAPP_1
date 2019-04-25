5c9464952086456753d72662db2fc36a487063c4
Dims_M <dimsxm@yandex.ru>
2019-04-25 22:35:37 +0300

    3

--- XpuctAPP/Form1.cs
+++ XpuctAPP/Form1.cs
@@ -29,13 +29,18 @@ namespace XpuctAPP
           Process process =  Process.Start(new ProcessStartInfo
             {
                 FileName = "cmd",
-                Arguments = "/c time /t",
-                RedirectStandardOutput = true,
-                WindowStyle = ProcessWindowStyle.Hidden
+               // Arguments = "/c time /t", // получение текущего времени
+               // Arguments = "/c chcp 65001 &  ipconfig", // англ кодировка 65001 рус 1251 
+                Arguments = "/c chcp 65001 & dism /online /cleanup-image /startcomponentcleanup",
+              UseShellExecute = false, // не использовать текущию видемую оболочку 
+                CreateNoWindow = true, // команда на Несоздание окна
+                RedirectStandardOutput = true, // переопределение потока 
+               // WindowStyle = ProcessWindowStyle.Hidden
                 
 
             });
-            label1.Text = process.ToString();
+            label1.Text = process.StandardOutput.ReadToEnd();
+            //в лэйбл записываем полученные данные из консоли.
         }
 
         private void label1_Click(object sender, EventArgs e)
